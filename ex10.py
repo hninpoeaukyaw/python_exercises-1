b39783203ec50629336442910198bb0965459a89
PhyuAye <phyulay97.mmm.email>
2018-06-06 21:50:57 +0630

    ex10.py

---
+++ ex10.py
@@ -0,0 +1,13 @@
+tebby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat." 
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+print(tebby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
