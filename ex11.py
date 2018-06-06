4113ec2fe1d0d7e95941e0652a4966f1a936d152
PhyuAye <phyulay97.mmm.email>
2018-06-06 21:56:48 +0630

    ex11.py

---
+++ ex11.py
@@ -0,0 +1,7 @@
+print("How old are you?", end=' ')
+age= input()
+print("How tall are you?", end=' ')
+heigh = input()
+print("How much do you weigh?", end=' ')
+weigh = input()
+print(f"So, you're {age} old, {heigh} tall and {weigh} heavy.")
